c61fb6be616e0d4eb103d89d87befc758f8d951c
friend1ws <friend1ws@gmail.com>
2022-09-07 16:08:19 +0000

    refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use std::error::Error;
 use bio::io::fasta;
 
 fn main() -> Result<(),  Box<dyn Error>> {
+
     let input_file_path = env::args_os().nth(1).expect("No argument");
-
     let reader = fasta::Reader::from_file(input_file_path)?;
-
  
     for result in reader.records() {
         let record = result?;
-        let seq_vec = record.seq().to_vec();
+        let seq_vec = record.seq(); 
 
         // eprintln!("Processing {}", record.id());
 
@@ -18,7 +17,7 @@ fn main() -> Result<(),  Box<dyn Error>> {
         let mut prev_chr: u8 = 0;
         for temp_chr_ptr in seq_vec.iter() {
             if *temp_chr_ptr != prev_chr && prev_chr != 0 {
-                comp_seq.push(prev_chr)
+                comp_seq.push(prev_chr);
             } 
             prev_chr = *temp_chr_ptr;
  
@@ -29,7 +28,6 @@ fn main() -> Result<(),  Box<dyn Error>> {
         println!(">{}\n{}", record.id(), comp_seq_str);
 
     }
-
 
     Ok(())
 }
